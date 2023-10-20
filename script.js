d0f795a80a248e46abde5b2ce5c2c0699c1eddcb
Fatemeh madihi <54662177+fmadihi@users.noreply.github.com>
2023-10-20 10:04:49 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
+
 $(document).ready(function () {
     /*let hour = Number(document.getElementById("hour").innerText);
     let minute = Number(document.getElementById("minute").innerText);
     let second = Number(document.getElementById("second").innerText);
     let milisecond = Number(document.getElementById("milisecond").innerText);*/
-    var hourr = 00;
-    var minute =00;
-    var second =00;
+    var hour = 00;
+    var minute = 00;
+    var second = 00;
     var milisecond = 00;
     var stFunc;
     $(".reset").click(function () {
         clearInterval(stFunc);
         $("#hour,#minute,#second,#milisecond").text("00");
+        hour = 00;
+        minute = 00;
+        second = 00;
+        milisecond = 00;
     })
     $(".start").click(function () {
-           stFunc =  setInterval(startFunc, 10);
+        stFunc = setInterval(startFunc, 10);
     })
-    $(".end").click(function (){
+    $(".end").click(function () {
         clearInterval(stFunc);
     })
     function startFunc() {
@@ -31,9 +36,9 @@ $(document).ready(function () {
                     $("#minute").text(minute);
                     second = 0;
                     if (minute == 60) {
-                        hourr++;
+                        hour++;
                         minute = 0;
-                        $("#hour").text(hourr);
+                        $("#hour").text(hour);
                         $("#minute").text(minute);
                         $("#second").text(second);
                     }
@@ -42,4 +47,5 @@ $(document).ready(function () {
         }
 
     }
+    console.log(typeof hour, minute, second, milisecond);
 })
