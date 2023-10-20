b1db10a63f35bb568ce9d14bd4a685ca627061d1
Fatemeh madihi <54662177+fmadihi@users.noreply.github.com>
2023-10-20 09:34:09 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,45 @@
-
 $(document).ready(function () {
     /*let hour = Number(document.getElementById("hour").innerText);
     let minute = Number(document.getElementById("minute").innerText);
     let second = Number(document.getElementById("second").innerText);
     let milisecond = Number(document.getElementById("milisecond").innerText);*/
-    var hour=00;
-    var minute = 00;
+    var hourr = 00;
+    var minute =00;
     var second =00;
-    var milisecond=00;
+    var milisecond = 00;
+    var stFunc;
     $(".reset").click(function () {
+        clearInterval(stFunc);
         $("#hour,#minute,#second,#milisecond").text("00");
     })
     $(".start").click(function () {
-        if (hour == 0 && minute == 0 && second == 0 && milisecond == 0) {
-            setInterval(startFunc, 10);
-        }
-        
-
+           stFunc =  setInterval(startFunc, 10);
+    })
+    $(".end").click(function (){
+        clearInterval(stFunc);
     })
     function startFunc() {
-        if(milisecond<100){
+        if (milisecond < 100) {
             milisecond++;
-        $("#milisecond").text(milisecond);
-        }
-        
-        if(milisecond==100){
-            second++;
-            $("#second").text(second);
-            milisecond=0;
-            if(second==60){
-                minute++;
-            $("#minute").text(minute);
-            second=0;
+            $("#milisecond").text(milisecond);
+            if (milisecond == 100) {
+                second++;
+                $("#second").text(second);
+                milisecond = 0;
+                if (second == 60) {
+                    minute++;
+                    $("#minute").text(minute);
+                    second = 0;
+                    if (minute == 60) {
+                        hourr++;
+                        minute = 0;
+                        $("#hour").text(hourr);
+                        $("#minute").text(minute);
+                        $("#second").text(second);
+                    }
+                }
             }
         }
 
     }
-    console.log(typeof hour, minute, second, milisecond);
-
 })
